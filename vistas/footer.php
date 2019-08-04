6fee0afebf78f14ff8b60d9911308afab6e1d31a
Francisco Alcaraz <fran.alcaraz@live.com>
2019-08-03 23:03:09 -0300

    manija

--- vistas/footer.php
+++ vistas/footer.php
@@ -1,3 +1,4 @@
+<!--
 				<footer id="footer">
 					<div id="containerfoo">
 						<div id="onefoo" >
@@ -12,4 +13,68 @@
 							<a href="https://www.smoktech.com/"><img src="img/general/smok_logo.png" alt=""></a>
 						</div>
 					</div> 
-				</footer>
+				</footer>-->
+<style>
+    footer{
+    color: white;
+    margin-top: 25px;
+    padding-top: 25px;
+    }
+    a{
+    color: white;
+    text-decoration: none;
+    }
+    a:hover{
+        color: white;
+        text-decoration: underline;
+    }
+    .linkdv{
+        color: white !important;
+        text-decoration: none;
+    }
+  .nombreArt {
+  color: black !important;
+}
+</style>
+        <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+   <footer class="page-footer font-small bg-primary justify-content-center">
+    <div class="container-fluid text-center text-md-left">
+      <div class="row">
+        <div class="col-6 text-center">
+          <p><a href="_admin/" class="pdf">Administrar</a></p>
+          <p>eCommerce que no persigue fines de lucro, unicamente propositos educativos (?)</p>
+          <a href="files/terms.pdf" target="_blank" class="pdf">Ver Catalogo</a> -
+            <a href="files/terms.pdf" download="terminos" class="pdf">Descargar Catalogo</a> -
+          
+          </div>
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <div class="col-6 text-center">
+           
+            <h5>Nuestras Marcas</h5>
+
+            <ul class="list-unstyled">
+              
+                <a href="http://www.vaporesso.com"><img src="img/general/vaporeso_logo.png" alt="vaporesso logo"></a>
+           
+                <a href="http://www.smok.com"><img src="img/general/smok_logo.png" alt="smok logo"></a>
+             
+             
+                <a href="http://www.elementeliquids.com"><img src="img/general/element_logo.png" alt="element logo"></a>
+              
+             
+                <a href="http://www.charlieschalkdust.com"><img src="img/general/ccd_logo.png" alt="ccd logo"></a>
+              
+            </ul>
+          </div>
+          </div>
+
+    </div>
+
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="https://davinci.edu.ar" id="linkdv">Escuela de Arte Multimedial Da Vinci</a>
+    </div>
+
+
+  </footer>
